a5ebea701b4f37877ed9a3ea565200df745c80b9
Dungna89 <dungna29@fpt.edu.vn>
2021-09-15 09:18:05 +0700

    1

---
+++ 1_WE17202_MOB1014_FALL21/src/Buoi1_LamQuenJava_KhaiBaoBien/S1_PhimTatNetbeans.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buoi1_LamQuenJava_KhaiBaoBien;
+
+/**
+ *
+ * @author Dungna89
+ */
+public class S1_PhimTatNetbeans {
+
+  /*
+      1.//: Comment trên 1 dòng
+      2.ALT + Quộc chuột lên xuống: Zoom Code 
+      3.1 Chương trình cần chạy được cần phải có Main và chỉ có 1 Main duy nhất
+      4.psvm + tab: Gọi nhanh main
+      5.main + tab: Gọi nhanh main
+      6.Ctrl + Space: Gợi ý code
+      7.sout + tab: In ra màn hình
+      8.Chuột phải vào bên trong file code chọn Run File: Chạy chương trình
+      9.Shilft + F6: Run File
+      10.Ctrl + Shift + Mũi tên lên xuống: Nhân bản dòng code nhanh
+      11.Ctrl + /: Comment code 1 hoặc nhiều dòng
+      12.Alt + Shilft + F: Clean Code
+   */
+ /*
+    Có 2 loại người học lập trình:
+      1. Người học chép code
+      2. Người code thực thụ
+   */
+  public static void main(String[] args) {
+    System.out.println("Chào các bạn đang học java 1");
+    
+    //Bài 1: Viết 1 chương trình in ra các thông tin cá nhân: Tên, Tuổi, Ngành Học, Quê Quán trên từng dòng và in ra màn hình
+    System.out.println("Dũng");
+    System.out.println("3x Tuổi");
+    System.out.println("UDPM");
+    System.out.println("HN");
+  }
+}
---
+++ 1_WE17202_MOB1014_FALL21/src/Buoi1_LamQuenJava_KhaiBaoBien/S2_KhaiBaoBien.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buoi1_LamQuenJava_KhaiBaoBien;
+
+/**
+ *
+ * @author Dungna89
+ */
+public class S2_KhaiBaoBien {
+  /*
+    *********** Biến khái niệm cơ bản ***********
+    Định nghĩa: Biến dùng để lữu trữ giá trị
+    Công thức: <kiểu dữ liệu> <tên biến> [=giá trị khởi đầu];
+    <kiểu dữ liệu>: Định nghĩa kiểu dữ liệu cho biến
+    <tên biến>: Dùng để phân biệt giữa các tên biến với nhau và có phân biệt chữ hoa chữ thường
+  
+   *********** Quy Tắc Đặt Tên Biến ***********        
+        Tên biến có phân biệt chữ Hoa và chữ thường
+         String firstname;// Kiểu Normal  //Sử dụng khi tên biến có 2 từ trở lên
+         String FirstName; //Kiểu CamelCase //Sử dụng khi tên biến có 2 từ trở lên
+         String first_name; // Kiểu Underscores //Sử dụng khi tên biến có 2 từ trở lên 
+         Lưu ý: đặt tên biến không được phép bắt đầu bởi số , có dấu gạch ngang, sử dụng từ khóa của java (int,if,long....)
+         int int = 5; //Vi phạm nguyên tắc đặt biến
+         Trong cùng 1 class thì biến phải sử dụng tên khác nhau và đặt sao cho đúng nghĩa đại diện
+  */
+    
+  public static void main(String[] args) {
+    
+  }
+}
